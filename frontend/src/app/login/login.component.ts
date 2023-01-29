0c7a902ee6f367abd43986eff969934b863ba4a6
Jimmy290901 <shubham290901@gmail.com>
2023-01-29 17:18:56 +0530

    Added Styles to mat-card

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  host: {
+    class: 'box'
+  },
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
